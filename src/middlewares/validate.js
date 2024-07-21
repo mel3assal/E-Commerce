76c9ed21f3783f7f71013573caf1ed71bcb9eb95
mel3assal <123838217+mel3assal@users.noreply.github.com>
2024-07-21 13:49:32 +0300

    validation and file upload

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,11 +1,22 @@
 import { AppError } from './../utilis/AppError.js';
-export const validate=(schema)=>{
-    return async (req,res,next)=>{
-        let {error}=schema.validate({images:req.files,image:req.file,...req.body,...req.params},{abortEarly:false})
-        if(!error){
-            next()
-        }else {let errMsgs=error.details?.map(err=>err.message)
-        next(new AppError(errMsgs,401))}
-
+export const validate = (schema) => {
+    return async (req, res, next) => {
+        let files = () => {
+            if (req.file) {
+                let { fieldname } = req.file
+                return { fieldname: req.file }
+            }
+        if (req.files) {
+            return { images: req.files }
+        }
     }
+    let { error } = schema.validate({ ...files(), ...req.body, ...req.params }, { abortEarly: false })
+    if (!error) {
+        next()
+    } else {
+        let errMsgs = error.details?.map(err => err.message)
+        next(new AppError(errMsgs, 401))
+    }
+
+}
 }
